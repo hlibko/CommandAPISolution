8db461b5037019ab1ff20b482c50922ac6854137
Hlib Kochubei <hlib.kochubei@gmail.com>
2022-11-25 11:11:57 +0100

    Add CommandsController

--- CommandAPI/Program.cs
+++ CommandAPI/Program.cs
@@ -1,9 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/", () => "Hello!");
+app.MapControllers();
 
 app.Run();
