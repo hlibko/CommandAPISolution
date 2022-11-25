beba43f4e0364d612db3a913ceb5cbdcd945c3c4
Hlib Kochubei <hlib.kochubei@gmail.com>
2022-11-25 21:33:39 +0100

    Add ConnectionStrings

--- CommandAPI/Program.cs
+++ CommandAPI/Program.cs
@@ -1,8 +1,14 @@
 using CommandAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+var postgreSqlConnection = builder.Configuration.GetConnectionString("PostgreSqlConnection");
+builder.Services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(postgreSqlConnection));
+
+
 builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
 
 var app = builder.Build();
