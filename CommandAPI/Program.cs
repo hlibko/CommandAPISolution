c14f59797f3d45171c556568935c07a75bbd1e27
Hlib Kochubei <hlib.kochubei@gmail.com>
2022-11-26 17:35:34 +0100

    Add User Secrets

--- CommandAPI/Program.cs
+++ CommandAPI/Program.cs
@@ -1,12 +1,16 @@
 using CommandAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-var postgreSqlConnection = builder.Configuration.GetConnectionString("PostgreSqlConnection");
-builder.Services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(postgreSqlConnection));
+var npgsqlConBuilder = new NpgsqlConnectionStringBuilder();
+npgsqlConBuilder.ConnectionString = builder.Configuration.GetConnectionString("PostgreSqlConnection");
+npgsqlConBuilder.Username = builder.Configuration["UserID"];
+npgsqlConBuilder.Password = builder.Configuration["Password"];
+builder.Services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(npgsqlConBuilder.ConnectionString));
 
 //builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
 builder.Services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
