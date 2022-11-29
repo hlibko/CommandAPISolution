080bd4db0cf9ed3aba3b2092958a87dc036c6eb8
Hlib Kochubei <hlib.kochubei@gmail.com>
2022-11-29 14:15:49 +0100

    Add Tests

---
+++ CommandAPI.Tests/CommandTests.cs
@@ -0,0 +1,53 @@
+﻿namespace CommandAPI.Tests
+{
+    public class CommandTests : IDisposable
+    {
+        Command testCommand;
+
+        public CommandTests()
+        {
+            //Arrange
+            testCommand = new Command
+            {
+                HowTo = "Do something awesome",
+                Platform = "xUnit",
+                CommandLine = "dotnet test"
+            };
+        }
+
+        public void Dispose()
+        {
+            testCommand = null;
+        }
+
+        [Fact]
+        public void CanChangeHowTo()
+        {
+            //Act
+            testCommand.HowTo = "Execute Unit Tests";
+
+            //Assert
+            Assert.Equal("Execute Unit Tests", testCommand.HowTo);
+        }
+
+        [Fact]
+        public void CanChangePlatform()
+        {
+            //Act
+            testCommand.Platform = "Platform";
+
+            //Assert
+            Assert.Equal("Platform", testCommand.Platform);
+        }
+
+        [Fact]
+        public void CanChangeCommandLine()
+        {
+            //Act
+            testCommand.CommandLine = "Command Line";
+
+            //Assert
+            Assert.Equal("Command Line", testCommand.CommandLine);
+        }
+    }
+}
