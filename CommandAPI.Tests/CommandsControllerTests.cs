3a578267171439b35e0ed1e214bf8569999e789b
Hlib Kochubei <hlib.kochubei@gmail.com>
2022-12-17 22:35:24 +0100

    Error tests

--- CommandAPI.Tests/CommandsControllerTests.cs
+++ CommandAPI.Tests/CommandsControllerTests.cs
@@ -277,7 +277,7 @@ namespace CommandAPI.Tests
             var result = controller.DeleteCommand(0);
 
             //Assert
-            Assert.IsType<NotFoundResult>(result);
+            Assert.IsType<OkResult>(result);
 
         }
 
